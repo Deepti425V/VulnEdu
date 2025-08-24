dfcdf68756fbd7cea82f6198e87b91c3336fc300
Deepti Veldanda <veldandad@findlay.edu>
2025-08-24 17:45:28 -0400

    REPLACED

--- vulnedu.py
+++ vulnedu.py
@@ -564,4 +564,5 @@ def cve_detail(cve_id):
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("[APP] Starting VulnEdu...")
+    app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
